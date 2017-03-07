8f1d7d40c0dc6122763a0d6a72884a9ebf27fa74
Christian Stade-Schuldt <christian.stade-schuldt@project-a.com>
2017-03-07 11:48:46 +0100

    update pycriteo library

--- setup.py
+++ setup.py
@@ -8,12 +8,8 @@ setup(
 
     install_requires=[
         'click>=6.0',
-        'pycriteo<=0.0.2'
+        'pycriteo==0.0.2'
     ],
-
-    dependency_links=[
-        'https://github.com/fdellavedova/pycriteo/archive/373609b47976fc3f637f053c9f14575158ee843a.zip#egg=pycriteo-0.0.2'],
-
     packages=find_packages(),
 
     author='Mara contributors',
